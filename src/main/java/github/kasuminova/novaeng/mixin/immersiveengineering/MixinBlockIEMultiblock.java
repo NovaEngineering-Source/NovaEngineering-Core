6d06b351820753d5e737560c0e90f382f915c8ba
Hikari_Nova <3044344887@qq.com>
2024-03-15 17:34:29 +0800

    - 锐评 ImmersiveEngineering 的铸币结构破坏代码，产出掉落物不清空内部物品栏。

--- src/main/java/github/kasuminova/novaeng/mixin/immersiveengineering/MixinBlockIEMultiblock.java
+++ src/main/java/github/kasuminova/novaeng/mixin/immersiveengineering/MixinBlockIEMultiblock.java
@@ -1,10 +1,10 @@
 package github.kasuminova.novaeng.mixin.immersiveengineering;
 
 import blusunrize.immersiveengineering.common.blocks.BlockIEMultiblock;
+import blusunrize.immersiveengineering.common.blocks.TileEntityMultiblockPart;
 import blusunrize.immersiveengineering.common.util.inventory.IIEInventory;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.NonNullList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,11 +22,11 @@ public class MixinBlockIEMultiblock {
             at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", remap = false)
     )
     @SuppressWarnings("rawtypes")
-    private boolean onGetDrops(final Iterator instance, @Local(name = "tileEntity") TileEntity tile) {
+    private boolean onBreakBlock(final Iterator instance, @Local(name = "master") TileEntityMultiblockPart master) {
         if (instance.hasNext()) {
             return true;
         }
-        if (tile instanceof IIEInventory ieInv) {
+        if (master instanceof IIEInventory ieInv) {
             NonNullList<ItemStack> inventory = ieInv.getInventory();
             Collections.fill(inventory, ItemStack.EMPTY);
         }
